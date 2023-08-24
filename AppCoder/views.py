09871d5879272452c485f23ed3031ce9ba4397a6
Axl R. Penaloza O <penalozarafael21@gmail.com>
2023-08-23 21:34:53 -0300

    se agregaron las views de inicio, cursos, profesores, estudiantes, entregables

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -10,3 +10,22 @@ def curso(req, nombre, camada):
     return HttpResponse(f"""
         <p>Curso: {curso.nombre} - Camada: {curso.camada} agregado!! </p>
         """)  
+
+def inicio(req):
+    return HttpResponse("vista inicio")
+
+
+def cursos(req):
+    return HttpResponse("vista cursos")
+
+
+def profesores(req):
+    return HttpResponse("vista profesores")
+
+
+def estudiantes(req):
+    return HttpResponse("vista estudiantes")
+
+
+def entregables (req):
+    return HttpResponse("vista entregables")
