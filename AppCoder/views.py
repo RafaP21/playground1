c2ded72bd80ba64028873d76e62d2e6280807651
Axl R. Penaloza O <penalozarafael21@gmail.com>
2023-08-23 23:43:11 -0300

    agregamos los colores a los templates

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -17,20 +17,19 @@ def lista_Cursos(req):
     return render(req,"lista_cursos.html", {"lista_cursos" : lista})
 
 def inicio(req):
-    return HttpResponse("vista inicio")
+    return render(req, "inicio.html")
 
 
 def cursos(req):
-    return HttpResponse("vista cursos")
-
+    return render(req, "cursos.html")
 
 def profesores(req):
-    return HttpResponse("vista profesores")
+    return render(req, "profesores.html")
 
 
 def estudiantes(req):
-    return HttpResponse("vista estudiantes")
+    return render(req, "estudiantes.html")
 
 
 def entregables (req):
-    return HttpResponse("vista entregables")
+    return render(req, "entregables.html")
