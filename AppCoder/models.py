b08650c048eaccb1526274d4ae4536d341473a23
Axl R. Penaloza O <penalozarafael21@gmail.com>
2023-09-08 11:35:12 -0300

    foreing keys y __str__ de los modelos

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -6,18 +6,29 @@ class Curso (models.Model):
     nombre = models.CharField(max_length=50)
     camada = models.IntegerField()
 
+    def __str__(self):
+         return f"{self.nombre} {self.camada}"
+
 class Estudiante (models.Model):
      nombre = models.CharField(max_length=30)
      apellido = models.CharField(max_length=30)
      email = models.EmailField(null=True)
+     
+     def __str__(self):
+          return f"{self.nombre} {self.apellido}"
 
 class Profesor (models.Model):
      nombre = models.CharField(max_length=30)
      apellido = models.CharField(max_length=30)
      email = models.EmailField(null=True) 
      profecion = models.CharField(max_length=30, null=True)
+     cursos = models.ManyToManyField(Curso)
+
+     def __str__(self):
+          return f"{self.nombre} {self.apellido}"
 
 class Entregable(models.Model):
      nombre = models.CharField(max_length=30)
      fechaDeEntrega = models.DateField()
      entregado = models.BooleanField()
+     estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE, null=True)
